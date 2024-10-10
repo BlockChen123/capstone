a45fd2377cb82c1cde0afc941c1708a227df5156
block.chen123@gmail.com <block.chen123@gmail.com>
2024-10-09 20:35:38 -0700

    Initial commit on main

---
+++ ValueDemo5.java
@@ -0,0 +1,17 @@
+public class ValueDemo5{
+	public static void main(String[] args){
+		String name="送初恋回家";
+		
+		String actors="刘鑫 张雨提 高媛";
+		
+		int year = 2020;
+		
+		double rate =9.0;
+		
+		System.out.println(name);
+		System.out.println(actors);
+		System.out.println(year);
+		System.out.println(rate);
+		
+		}	
+}
